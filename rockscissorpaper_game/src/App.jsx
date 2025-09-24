277be0d8320a883b595498d131ba38975ceaa218
Jaehee Roh <snowcat3b@gmail.com>
2025-09-24 21:08:16 +0900

    game

--- rockscissorpaper_game/src/App.jsx
+++ rockscissorpaper_game/src/App.jsx
@@ -6,14 +6,14 @@ import Box from "./component/Box.jsx"
 
 const choice = {
     rock:{
-      name:"Rock",
+      name:"rock",
       img:"https://store.clickhole.com/cdn/shop/files/Untitleddesign_6.png?v=1693423886"
     },
     scissors:{
-      name:"Scissors",
+      name:"scissors",
       img:"https://m.media-amazon.com/images/I/51QtmdQD-sL._UF894,1000_QL80_.jpg"    },
     paper:{
-      name:"Paper",
+      name:"paper",
       img:"https://m.media-amazon.com/images/I/61OorFhm6SL._UF894,1000_QL80_.jpg"
     }
   }
@@ -28,18 +28,76 @@ function App() {
   // 상태 결과에 따라 테투리 색이 바뀐다. (이기면 초록, 지면 빨강, 비기면 검정)
 
   const [userSelect, setUserSelect] = useState(null)
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState("")
   // state인 userSelect가 변할때마다 ui가 변한다!
   // state는 그냥 변수처럼 = 으로 못바꿈. set함수를 무조건 사용해야한다!
+
+
   const play=(userChoice)=>{
-    console.log("선택됌", userChoice)
+    // console.log("선택됌", userChoice)
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice()
+    // console.log("computerChoice", computerChoice);
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+    console.log(judgement(choice[userChoice], computerChoice))
+    // judgement는 일반함수. 따라서 state처럼 비동기적으로 실행되는게 아니라 콘솔에도 누르는 값이 바로바로 반영돼서 보인다.
+    // state는 렌더링 이후 최신화 된다.
+  }
+
+  const randomChoice = ()=>{
+    let itemArray = Object.keys(choice)
+    // Object.keys() -> 객체의 키 값만 뽑아서 array로 만들어주는 함수.
+    // console.log("array",itemArray);
+    let randomItem = Math.floor(Math.random()*itemArray.length)
+    let final = itemArray[randomItem]
+    return choice[final];
+    // console.log(Math.floor(Math.random()*3));
+  }
+  
+  const judgement = (user, computer)=>{
+    console.log(user, computer);
+
+    // if(userChoice === computerChoice){
+    //   비김
+    // } else{
+    //   if(userChoice == "scissors"){
+    //     if(computerChoice == "paper"){
+    //       user가 이김
+    //     } else{
+    //       컴퓨터가 이김
+    //     }
+    //   } else if(userChoice == "rock"){
+    //     if(computerChoice == "scissors"){
+    //       user가 이김
+    //     } else{
+    //       컴퓨터가 이김
+    //     }
+    //   } else if(userChoice == "paper"){
+    //     if(computerChoice == "rock"){
+    //       user가 이김
+    //     } else{
+    //       컴퓨터가 이김
+    //     }
+    //   }
+    // }
+
+    if(user.name === computer.name){
+      return "tie";
+    } else{
+      if(user.name == "scissors") return computer.name == "paper"?"win":"lose";
+      else if(user.name == "rock") return computer.name == "scissors"?"win":"lose";
+      else if(user.name == "paper") return computer.name == "rock"?"win":"lose";
+    }
+
   }
 
   return (
     <div className='wrapper'>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        <Box title="Computer"/>
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
       </div>
       <div className='main'>
         <button onClick={() => play("scissors")}>가위</button>
@@ -54,3 +112,4 @@ function App() {
 export default App
 
 // 함수 실행시 함수명() 이렇게 있으면 리액트가 그냥 바로 함수를 실행시켜 버린다. 따라서 콜백함수 형태. 즉 () => 함수() 이런 방식으로 써줘야함!
+
--- rockscissorpaper_game/src/component/Box.jsx
+++ rockscissorpaper_game/src/component/Box.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 
 const Box = (props) => {
 
+    let result;
+    if(props.title === "Computer" &&
+        props.result !== "tie" &&
+        props.result !== ""
+    ){
+        result = props.result === "win"?"lose":"win";
+    } else {
+        result = props.result;
+    }
+
     const initialImg = "https://mblogthumb-phinf.pstatic.net/MjAyMTA3MjJfMTUy/MDAxNjI2OTE3NjI5MzIw.MpddVnh8PLVpQBq4FiP00-m8NPulTwNmWrMIKyPys1Yg.sZlzccrSUkJXLkEdy4u2IFPY7ssSb2KpDgg4YCMWkaAg.PNG.dct8867/%EB%AC%BC%EC%9D%8C%ED%91%9C_%EB%A0%88%EB%93%9C_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B84x.png?type=w800";
     console.log(props)
     return (
-        <div className="box">
+        <div className={`box ${result}`}>
         <h1>{props.title}</h1>
         <img className="img-size" src={props.item?props.item.img:initialImg}/>
-        <h2>Win</h2>
+        <h2>{result}</h2>
         </div>
     )
+    // win 일때는 초록색. lose 일때는 빨간색. tie 일때 검은색.
 }
 
 export default Box
