337497f416af596c1e9b378f1077ac68a041f6b8
Jaehee Roh <snowcat3b@gmail.com>
2025-10-11 01:04:14 +0900

    edit

--- router-example/src/page/ProductDetailPage.jsx
+++ router-example/src/page/ProductDetailPage.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 const ProductDetailPage = () => {
     const {id} = useParams()
     // destructuring 이용!
-    console.log("params", {id})
+    console.log("params", id)
   return (
     <div>
       <h1>product Detail{id}</h1>
